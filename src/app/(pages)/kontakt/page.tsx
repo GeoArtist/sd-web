2b1158c1e0ed21d8a51f9bf24c01ecd38c71d286
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2024-11-12 09:36:30 +0100

    update form headers, rodo, styles

--- src/app/(pages)/kontakt/page.tsx
+++ src/app/(pages)/kontakt/page.tsx
@@ -33,30 +33,42 @@ export default function KontaktPage() {
  
     return (
         <>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name">Imię: </label>
-            <input id="name" type="text" placeholder="Podaj Twoje imię" {...register("name")}  />
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+            <h3>Masz pytania?</h3>
+            <h4>Skontaktuj się z nami</h4>
+            <label htmlFor="name" className={styles.form__label}>Imię: </label>
+            <input id="name" type="text" placeholder="Podaj Twoje imię" {...register("name")} className={styles.form__input} />
             <p className={styles.error}>{errors.name?.message}</p>
 
-            <label htmlFor="email">e-mail: </label>
-            <input id="email" type="text"placeholder="Podaj Twój adres e-mail" {...register("email")}  />
+            <label htmlFor="email" className={styles.form__label}>Email: </label>
+            <input id="email" type="text"placeholder="Podaj Twój adres e-mail" {...register("email")} className={styles.form__input} />
             <p className={styles.error}>{errors.telephoneOremail?.message || errors.email?.message}</p>
 
-            <label htmlFor="telephone">Telefon: </label>
-            <input id="telephone" type="text" placeholder="Podaj Twój numer telefonu" {...register("telephone")}  />
+            <label htmlFor="telephone" className={styles.form__label}>Telefon: </label>
+            <input id="telephone" type="text" placeholder="Podaj Twój numer telefonu" {...register("telephone")} className={styles.form__input} />
             <p className={styles.error}>{errors.telephone?.message}</p>
 
-            <label htmlFor="message">Wiadomość: </label>
-            <textarea id="message" placeholder="Wpisz treść wiadomości" {...register("message")}  />
+            <label htmlFor="message" className={styles.form__label}>Wiadomość: </label>
+            <textarea id="message" placeholder="Wpisz treść wiadomości" {...register("message")} className={styles.form__textarea} />
             <p className={styles.error}>{errors.message?.message}</p>
             
-            <input type="checkbox"  id='rodo-permission' {...register("rodo_permission")}/>
-            <label htmlFor="rodo-permission">Dane kontaktowe podaję dobrowolnie oraz wyrażam zgodę na kontakt i przetwarzanie danych w celu uzyskania odpowiedzi od SOFT-DATA Marek Szczepkowski, ul. Marii Dąbrowskiej 13/52, 10-685 Olsztyn, zgodnie z regulaminem</label>
+           
+            {/* RODO PERMISSIONS */}
+            <div className={styles.form__rodo}>
+                <input type="checkbox"  id='rodo-permission' {...register("rodo_permission")} /> 
+                <label htmlFor="rodo-permission">
+                    Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji kontaktu z firmą 
+                    <span>SOFT-DATA Marek Szczepkowski</span> 
+                    (zgodnie z ustawą z dnia 10 maja 2018r. o ochronie danych osobowych (Dz. Ustaw z 2018, poz. 1000) 
+                    oraz zgodnie z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016r. 
+                    w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych)). 
+                </label>
             <p className={styles.error}>{errors.rodo_permission?.message}</p>
-
+            </div>    
+          
             <button>Wyślij</button>
-
         </form>
+
         {/* Send status */}
         {sendStatus.status!==0 && <p className={sendStatus.status===200 ? styles.success : styles.error}>{sendStatus.message}</p>}
         
