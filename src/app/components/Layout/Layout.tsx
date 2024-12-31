86f50b08a178b2eefc9e4279664ab45e65f02be6
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2024-12-31 11:57:14 +0100

    add gradient background

--- src/app/components/Layout/Layout.tsx
+++ src/app/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 
-import { Footer } from '../Footer/Footer';
-import { TopBar } from '../TopBar/TopBar';
-import { MainContent } from '../MainContent/MainContent';
+import { Footer } from "@/components/Footer/Footer";
+import { TopBar } from "@/components/TopBar/TopBar";
+import { MainContent } from "@/components/MainContent/MainContent";
 import {CurrentPathProvider} from '@/contexts/CurrentPathContext';
 type LayoutProps= {
     children: React.ReactNode;
