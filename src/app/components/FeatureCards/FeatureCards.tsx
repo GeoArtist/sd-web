9d9866daa43e10f2f9d10155aa58c8428666ce6e
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-01-01 11:03:54 +0100

    add bgc image to feature cards

--- src/app/components/FeatureCards/FeatureCards.tsx
+++ src/app/components/FeatureCards/FeatureCards.tsx
@@ -1,15 +1,30 @@
-import { featureCards } from '@/constants/featureCards';
-import styles from './FeatureCards.module.scss';
-import Card from '@/components/FeatureCard/FeatureCard';
+import { featureCards } from "@/constants/featureCards";
+import Card from "@/components/FeatureCard/FeatureCard";
+import bgc from "@/public/images/feature_cards_bgc.jpg";
+import Image from "next/image";
 
+import styles from "./FeatureCards.module.scss";
 
-export default function FeatureCards(){
-
-    return <>
-    <div className={styles.cards__wrapper}>
+export default function FeatureCards() {
+  return (
+    <>
+      <div className={styles.cards__wrapper}>
+        <Image
+          className={styles.bgc__img}
+          src={bgc}
+          alt="feature cards background"
+        />
         {featureCards.map((card) => {
-                    return <Card key={card.title} title={card.title} icon={card.icon} description={card.description}/>
-                })}
-    </div>
-        </>
+          return (
+            <Card
+              key={card.title}
+              title={card.title}
+              icon={card.icon}
+              description={card.description}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
