ef341766995a2dd605b15f9260041bc15239dbad
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-01-07 07:04:55 +0100

    refactor main page location

---
+++ src/app/(pages)/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from "../components/Loader/Loader";
+import styles from "./page.module.scss";
+export default function Loading() {
+  return (
+    <>
+      <div className={styles.loader}>
+        <Loader />
+      </div>
+    </>
+  );
+}
