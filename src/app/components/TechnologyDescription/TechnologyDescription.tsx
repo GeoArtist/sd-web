2f4c467f1f49d6796a6d90117b293cb383b96d2e
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-01-07 07:04:25 +0100

    refactor technologyview

---
+++ src/app/components/TechnologyDescription/TechnologyDescription.tsx
@@ -0,0 +1,34 @@
+import styles from "./TechnologyDescription.module.scss";
+
+import InfitnieHorizontalScroll from "@/components/InfitnieHorizontalScroll/InfitnieHorizontalScroll";
+import {
+  imgsTechData,
+  imgsTechDb,
+  imgsTechGeo,
+  imgsTechTech,
+  imgsTechWeb,
+} from "@/constants/InfiniteScrollImgs";
+import { ImgsLinks } from "@/types/imgsLinks";
+
+export function TechnologyDescription() {
+  const wheelToImgsMap: { [key: string]: ImgsLinks[] } = {
+    Geodezja: imgsTechGeo,
+    "Bazy Danych": imgsTechDb,
+    "Analiza i Przetwarzanie Danych": imgsTechData,
+    Wizualizacje: imgsTechWeb,
+    Narzędzia: imgsTechTech,
+  };
+  return (
+    <>
+      <div className={styles.wrapper}>
+        {Object.entries(wheelToImgsMap).map(([title, imgs]) => (
+          <>
+            <h2>{title}</h2>
+            <InfitnieHorizontalScroll imgs={imgs} />
+            <hr />
+          </>
+        ))}
+      </div>
+    </>
+  );
+}
--- src/app/views/TechnologyView/TechnologyView.tsx
+++ src/app/views/TechnologyView/TechnologyView.tsx
@@ -1,42 +1,27 @@
-import AboutUsDescription from '@/app/components/AboutUsDescription/AboutUsDescription';
-import InfitnieHorizontalScroll from '@/components/InfitnieHorizontalScroll/InfitnieHorizontalScroll'
-import { imgsTechData, imgsTechDb, imgsTechGeo, imgsTechTech, imgsTechWeb} from '@/constants/InfiniteScrollImgs';
-import logoCut from '@/public/logos/logo_cut.png'
-import Image from 'next/image'
+import InfitnieHorizontalScroll from "@/components/InfitnieHorizontalScroll/InfitnieHorizontalScroll";
+import {
+  imgsTechData,
+  imgsTechDb,
+  imgsTechGeo,
+  imgsTechTech,
+  imgsTechWeb,
+} from "@/constants/InfiniteScrollImgs";
+import { SubPageHeader } from "@/app/components/SubPageHeader/SubPageHeader";
 
-import styles from './TechnologyView.module.scss'
-import { SubPageHeader } from '@/app/components/SubPageHeader/SubPageHeader';
+import styles from "./TechnologyView.module.scss";
+import { TechnologyDescription } from "@/app/components/TechnologyDescription/TechnologyDescription";
 
-export default function TechnologyView(){
-    return <>
-        <div className={styles.technologyView}>
-            <div className={styles.technology__header}>
-                <SubPageHeader title={"TECHNOLOGIE"}/>
-            </div>
-
-            <div className={styles.technology__content}>
-                
-                <h2>Geodezja</h2>
-                <InfitnieHorizontalScroll imgs={imgsTechGeo}/>
-                <hr/>
-                
-                <h2>Bazy Danych</h2>
-                <InfitnieHorizontalScroll imgs={imgsTechDb}/>
-                <hr/>
-                
-                <h2>Analiza i Przetwarzanie Danych</h2>
-                <InfitnieHorizontalScroll imgs={imgsTechData}/>
-                <hr/>
-                
-                <h2>Wizualizacje</h2>
-                <InfitnieHorizontalScroll imgs={imgsTechWeb}/> 
-                <hr/>
-                
-                <h2>Narzędzia</h2>
-                <InfitnieHorizontalScroll imgs={imgsTechTech}/>
-                <hr/>
-              
-            </div>
+export default function TechnologyView() {
+  return (
+    <>
+      <div className={styles.technologyView}>
+        <div className={styles.technology__header}>
+          <SubPageHeader title={"TECHNOLOGIE"} />
         </div>
+        <div className={styles.technology__content}>
+          <TechnologyDescription />
+        </div>
+      </div>
     </>
+  );
 }
