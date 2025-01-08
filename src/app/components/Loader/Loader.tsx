0af6a0a95cced1bb82f0ae816846f5a5552651bf
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-01-08 07:24:14 +0100

    add loading offer

--- src/app/components/Loader/Loader.tsx
+++ src/app/components/Loader/Loader.tsx
@@ -3,13 +3,27 @@ import styles from './Loader.module.scss';
 
 export default function Loader() {
     return (
-        <div className={styles.loader}>
-            <div className={styles.orbe} style={{"--index": 0} as React.CSSProperties}></div>
-            <div className={styles.orbe} style={{"--index": 1} as React.CSSProperties}></div>
-            <div className={styles.orbe} style={{"--index": 2} as React.CSSProperties}></div>
-            <div className={styles.orbe} style={{"--index": 3} as React.CSSProperties}></div>
-            <div className={styles.orbe} style={{"--index": 4} as React.CSSProperties}></div>
-
-        </div>
-    )
+      <div className={styles.loader__wrapper}>
+        <div
+          className={styles.orbe}
+          style={{ "--index": 0 } as React.CSSProperties}
+        ></div>
+        <div
+          className={styles.orbe}
+          style={{ "--index": 1 } as React.CSSProperties}
+        ></div>
+        <div
+          className={styles.orbe}
+          style={{ "--index": 2 } as React.CSSProperties}
+        ></div>
+        <div
+          className={styles.orbe}
+          style={{ "--index": 3 } as React.CSSProperties}
+        ></div>
+        <div
+          className={styles.orbe}
+          style={{ "--index": 4 } as React.CSSProperties}
+        ></div>
+      </div>
+    );
 }
