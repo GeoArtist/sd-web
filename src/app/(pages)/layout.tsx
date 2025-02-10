90cdaf7eb0dc40cee2a101f9fec8ccdd427789d4
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-02-10 07:01:47 +0100

    ga add + css

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -5,6 +5,7 @@ import { roboto } from "../fonts/fonts";
 import { Toaster } from "sonner";
 import { Layout } from "../components/Layout/Layout";
 import { pagesMetadata } from "@/constants/metatags";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = pagesMetadata["home"];
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           className="custom-toaster"
         />
       </body>
+      <GoogleAnalytics gaId="G-FYJF1529CR" />
     </html>
   );
 }
