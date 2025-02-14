92e572f3ca64f5d0fa7f698a57f4444f48218009
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-02-14 17:43:42 +0100

    fix prefetch link errors

--- src/app/components/MainMenu/MainMenu.tsx
+++ src/app/components/MainMenu/MainMenu.tsx
@@ -17,24 +17,79 @@ export  function MainMenu({mode, showSidebar}:MainMenuProps) {
     // Mobile toggle function
     const mobileFn = mode==='mobile' ? showMenuUpdate : undefined
 
-    return <>
-      <nav className={mode=='desktop'  ? styles.desktopMenu : `${styles.mobileMenu} ${showSidebar ? "displayMenu":""}`} >
-            {showSideBarMenu && <div className={styles.sidebarBGC} onClick={showMenuUpdate}></div>}
-            <ul >
-                <li><Link onClick={mobileFn}  href="/" className={currentPath === '/' ? styles.active : "" }>STRONA GŁÓWNA</Link></li>
-                <li onClick={mobileFn} 
-                    onMouseEnter={()=>toggleDropDown(true)} 
-                    onMouseLeave={()=>toggleDropDown(false)} 
-                    className={styles.dropDown}>
-                    <Link  href="/oferta" className={currentPath.includes('/oferta') ? styles.active : "" } >OFERTA</Link>
-                    {/* if sidebar dont render OfferMenu */}
-                    {mode=='desktop' && openDropDown && <OfferMenu display={openDropDown} menuType={'dropdown'}/>} 
-                </li>
-                <li><Link onClick={mobileFn}  href="/o-nas" className={currentPath === '/o-nas' ? styles.active : "" }>O NAS</Link></li>
-                <li><Link onClick={mobileFn}  href="/technologie" className={currentPath === '/technologie' ? styles.active : "" }>TECHNOLOGIE</Link></li>
-                <li><Link onClick={mobileFn}  href="/kontakt" className={currentPath === '/kontakt' ? styles.active : "" }>KONTAKT</Link></li>
-            </ul>
-            
+    return (
+      <>
+        <nav
+          className={
+            mode == "desktop"
+              ? styles.desktopMenu
+              : `${styles.mobileMenu} ${showSidebar ? "displayMenu" : ""}`
+          }
+        >
+          {showSideBarMenu && (
+            <div className={styles.sidebarBGC} onClick={showMenuUpdate}></div>
+          )}
+          <ul>
+            <li>
+              <Link
+                onClick={mobileFn}
+                href="/"
+                className={currentPath === "/" ? styles.active : ""}
+                prefetch={false}
+              >
+                STRONA GŁÓWNA
+              </Link>
+            </li>
+            <li
+              onClick={mobileFn}
+              onMouseEnter={() => toggleDropDown(true)}
+              onMouseLeave={() => toggleDropDown(false)}
+              className={styles.dropDown}
+            >
+              <Link
+                href="/oferta"
+                className={currentPath.includes("/oferta") ? styles.active : ""}
+                prefetch={false}
+              >
+                OFERTA
+              </Link>
+              {/* if sidebar dont render OfferMenu */}
+              {mode == "desktop" && openDropDown && (
+                <OfferMenu display={openDropDown} menuType={"dropdown"} />
+              )}
+            </li>
+            <li>
+              <Link
+                onClick={mobileFn}
+                href="/o-nas"
+                className={currentPath === "/o-nas" ? styles.active : ""}
+                prefetch={false}
+              >
+                O NAS
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={mobileFn}
+                href="/technologie"
+                className={currentPath === "/technologie" ? styles.active : ""}
+                prefetch={false}
+              >
+                TECHNOLOGIE
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={mobileFn}
+                href="/kontakt"
+                className={currentPath === "/kontakt" ? styles.active : ""}
+                prefetch={false}
+              >
+                KONTAKT
+              </Link>
+            </li>
+          </ul>
         </nav>
-    </>
+      </>
+    );
 }
