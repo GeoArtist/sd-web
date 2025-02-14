217e9073c5fc455c47147fc9f395abac3da57981
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-02-14 08:43:32 +0100

    remove metatag add font preload

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,11 +1,4 @@
-import { Metadata } from "next/types";
 import { MainView } from "@/views/MainView/MainView";
-
-export const metadata: Metadata = {
-  verification: {
-    google: "aaaa",
-  },
-};
 
 export default function Home() {
   return (
--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -1,15 +1,12 @@
-import { Inter, Roboto} from 'next/font/google'
+import { Roboto} from 'next/font/google'
  
-export const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
- 
-export const roboto = Roboto({
-    weight: ['100','400', '700'],
-    style: ['normal', 'italic'],
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-roboto',
 
-})
+
+export const roboto = Roboto({
+  weight: ["100", "400", "700"],
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  display: "swap",
+  variable: "--font-roboto",
+  preload: true,
+});
