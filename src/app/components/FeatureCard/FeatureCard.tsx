5a1ad7663b9f0e8e52f295534d82454779ceb2e9
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-02-15 05:56:33 +0100

    cursor feature card fix

--- src/app/components/FeatureCard/FeatureCard.tsx
+++ src/app/components/FeatureCard/FeatureCard.tsx
@@ -1,22 +1,21 @@
-import styles from './FeatureCard.module.scss';
-import {FeatureCard} from '@/types/featureCard'
-import Image from 'next/image'
+import { FeatureCard } from "@/types/featureCard";
+import Image from "next/image";
 
+import styles from "./FeatureCard.module.scss";
 
-export function Card({title, description, icon}:FeatureCard){
-
-    return (
-      <>
-        <div className={styles.card}>
-          <Image
-            src={icon}
-            alt={title}
-            className={styles.card__icon}
-            quality={75}
-          />
-          <p className={styles.card__title}>{title}</p>
-          <p className={styles.card__description}>{description}</p>
-        </div>
-      </>
-    );
+export function Card({ title, description, icon }: FeatureCard) {
+  return (
+    <>
+      <div className={styles.card}>
+        <Image
+          src={icon}
+          alt={title}
+          className={styles.card__icon}
+          quality={75}
+        />
+        <p className={styles.card__title}>{title}</p>
+        <p className={styles.card__description}>{description}</p>
+      </div>
+    </>
+  );
 }
