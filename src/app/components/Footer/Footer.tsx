67df3a81669a38ac11933443ae0c21071a79e339
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-02-16 13:46:28 +0100

    add versioning to app

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,73 +1,17 @@
-'use client'
-import React, { Fragment } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import logo from "@/public/icons/feather_white.svg";
-import { regulations } from "@/constants/regulations";
-import { socialLinks } from "@/constants/socialLinks";
-import { useCurrentPath } from "@/contexts/CurrentPathContext";
+import React from "react";
+
+import { FooterLinks } from "@/components/FooterLinks/FooterLinks";
+import { VersionTag } from "../VersionTag/VersionTag";
+
 import styles from "./Footer.module.scss";
 
-export function Footer(){
-    const year = new Date().getFullYear() 
-    const currentPath = useCurrentPath()
-    return (
-      <>
-        <footer className={styles.footer}>
-          <div className={styles.footer__wrapper}>
-            <section className={styles.footer__card}>
-              <Link href="/" prefetch={false}>
-                <Image className={styles.footer__logo} src={logo} alt="logo" />
-              </Link>
-              <Link href="/" prefetch={false}>
-                SOFT-DATA
-              </Link>
-              <p> © {year}</p>
-            </section>
-            <section className={styles.footer__links}>
-              <Link
-                href="/kontakt"
-                className={`${styles.footer__link} ${
-                  currentPath === "/kontakt" ? styles.active : ""
-                }`}
-                prefetch={false}
-              >
-                Kontakt
-              </Link>
-              {regulations.map((regulation, index) => {
-                return (
-                  <Fragment key={index}>
-                    <Link
-                      href={`/regulaminy/${regulation.path}`}
-                      className={`${styles.footer__link} ${
-                        currentPath === `/regulaminy/${regulation.path}`
-                          ? styles.active
-                          : ""
-                      }`}
-                      prefetch={false}
-                    >
-                      {regulation.navName}
-                    </Link>
-                  </Fragment>
-                );
-              })}
-            </section>
-            <section className={styles.footer__socials}>
-              {socialLinks.map((socialLink) => (
-                <Link
-                  href={socialLink.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  key={socialLink.url}
-                >
-                  <Image src={socialLink.icon} alt={socialLink.alt} />
-                </Link>
-              ))}
-            </section>
-          </div>
-        </footer>
-      </>
-    );
+export function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        <FooterLinks />
+        <VersionTag />
+      </footer>
+    </>
+  );
 }
-   
-
---
+++ src/app/components/VersionTag/VersionTag.tsx
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import styles from "./VersionTag.module.scss";
+
+const TAG_FILE_PATH = path.join(process.cwd(), "tags.json");
+
+interface Tag {
+  tag: string;
+  date: string;
+}
+
+async function getTagFromFile(): Promise<Tag | null> {
+  if (fs.existsSync(TAG_FILE_PATH)) {
+    const data = fs.readFileSync(TAG_FILE_PATH, "utf-8");
+    return JSON.parse(data);
+  }
+  return null;
+}
+
+export  async function VersionTag() {
+  const tag = await getTagFromFile();
+
+  return (
+    <div className = {styles.footer__version}>
+      {tag ? (
+        <p>
+          {tag.tag}
+        </p>
+      ) : null}
+    </div>
+  );
+}
