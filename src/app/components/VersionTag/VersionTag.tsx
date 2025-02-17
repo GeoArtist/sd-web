e93a490df843836f7a990d493f8109c6621437dc
Marek Szczepkowski <marek.szczepkowski@dotconnect.vc>
2025-02-17 05:51:45 +0100

    update json solution

--- src/app/components/VersionTag/VersionTag.tsx
+++ src/app/components/VersionTag/VersionTag.tsx
@@ -3,12 +3,14 @@ import path from "path";
 import { Tag } from "@/types/gitTags";
 import styles from "./VersionTag.module.scss";
 
-const TAG_FILE_PATH = path.join(process.cwd(), "tags.json");
+const TAG_FILE_PATH = path.join(process.cwd(),"src", "constants", "tags.json");
 
 
 async function getTagFromFile(): Promise<Tag | null> {
+
   if (fs.existsSync(TAG_FILE_PATH)) {
     const data = fs.readFileSync(TAG_FILE_PATH, "utf-8");
+    console.log(data);
     return JSON.parse(data);
   }
   return null;
